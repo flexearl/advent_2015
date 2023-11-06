ca50d88e62c6dd7e83b7180335532e028ce1ea15
Felix <flexearl@outlook.com>
2023-11-06 13:02:13 +0000

    Finished Day 6 part 1

---
+++ Day_6/Day_6.go
@@ -0,0 +1,73 @@
+package Day_6
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func Part_1(data string) {
+	grid := [1000][1000]bool{}
+
+	f, err := os.Open("Day_6/Day_6.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		text := scanner.Text()
+		splitted := strings.Split(text, " ")
+		startIndex := getIndex(splitted[len(splitted)-3])
+		endIndex := getIndex(splitted[len(splitted)-1])
+		if splitted[0] == "turn" {
+			turnOn := false
+			if splitted[1] == "on" {
+				turnOn = true
+			}
+			for i := startIndex[0]; i <= endIndex[0]; i++ {
+				for j := startIndex[1]; j <= endIndex[1]; j++ {
+					grid[i][j] = turnOn
+				}
+			}
+		} else {
+			for i := startIndex[0]; i <= endIndex[0]; i++ {
+				for j := startIndex[1]; j <= endIndex[1]; j++ {
+					grid[i][j] = !grid[i][j]
+				}
+			}
+		}
+	}
+	litCounter := 0
+	for i := 0; i < 1000; i++ {
+		for j := 0; j < 1000; j++ {
+			if grid[i][j] {
+				litCounter++
+			}
+		}
+	}
+	fmt.Println(litCounter)
+}
+
+func getNumber(numb string) int {
+	numb = strings.Replace(numb, ",", "", 0)
+	converted, err := strconv.Atoi(numb)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return converted
+}
+
+func getIndex(numb string) [2]int {
+	splitted := strings.Split(numb, ",")
+	firstNumber, err := strconv.Atoi(splitted[0])
+	secondNumber, err := strconv.Atoi(splitted[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	numbers := [2]int{firstNumber, secondNumber}
+	return numbers
+}
--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
-import (
-	"2015/Day_5"
-)
+import "2015/Day_6"
 
 func main() {
 	//content, err := os.ReadFile("Day_5/Day_5.txt")
 
 	//data := string(content)
-	Day_5.Part_2("ugknbfddgicrmopn")
+	Day_6.Part_1("ugknbfddgicrmopn")
 }
